40997608b866d557df78e9973aa1c6aa1714b773
KenLee <2533376457@qq.com>
2023-11-27 15:30:42 +0800

    添加初始函数

--- Lab5/08_PWM_motor -1127/Soft_Drive/platformtest/test.c
+++ Lab5/08_PWM_motor -1127/Soft_Drive/platformtest/test.c
@@ -10,7 +10,186 @@
 //初始化 时钟、xll、电机gpio、oled、按键
 void system_init(void)
 {
+}
 
+void motor_gpio_config(void)
+{
+    rcu_periph_clock_enable(RCU_GPIOA);
+    gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO_PIN_6);
+    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
+    gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO_PIN_7);
+    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_7);
+    gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO_PIN_8);
+    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_8);
+    gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO_PIN_9);
+    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
+}
+
+void motor4Z(void)
+{
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+}
+
+void motor4(void)
+{
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(3);
+}
+
+void motor8(void)
+{
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(2);
+}
+
+void EXIT2_IRQHandler(void)
+{
+    if (RESET != exti_interrupt_flag_get(USER1_KEY_EXTI_LINE)) {
+        if (motor_speed++ >=10)
+            motor_speed = 3;
+        exti_interrupt_flag_clear(USER1_KEY_EXTI_LINE);
+    }
+}
+
+void EXIT3_IRQHandler(void)
+{
+    if (RESET != exti_interrupt_flag_get(USER2_KEY_EXTI_LINE)) {
+        if (mode_select++ >= 2)
+            mode_select = 0;
+        exti_interrupt_flag_clear(USER2_KEY_EXTI_LINE);
+    }
+}
+
+
+void motor_fanz1(uint8_t speed)
+{
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+}
+
+void motor_zhez1(uint8_t speed)
+{
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_reset(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_reset(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_reset(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+    delay_1ms(speed);
+}
+
+void motor_stop(void)
+{
+    gpio_bit_set(GPIOC, GPIO_PIN_6);
+    gpio_bit_set(GPIOC, GPIO_PIN_7);
+    gpio_bit_set(GPIOC, GPIO_PIN_8);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
 }
 
 //基础 电机转动
